12a2fa28d969aea3e91ef179f7e5129e554d8146
nuthankalyan <dolfin.pft@gmail.com>
2025-04-10 22:39:05 +0530

    a

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin: [
     'http://localhost:3000',
+    'https://finaxial-application.vercel.app',
     'https://finaxial-client.onrender.com',
     'https://finaxial.onrender.com', // Add potential client URL
     process.env.CLIENT_URL, // For flexibility
